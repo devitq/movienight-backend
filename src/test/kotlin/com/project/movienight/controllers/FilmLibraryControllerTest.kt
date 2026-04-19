b7b9061ba6e8181b17248db5a1292b31e58bc8bc
Elena Ponomareva <eponomar9@gmail.com>
2026-04-19 15:46:59 +0300

    спринг тесты для контроллеров

---
+++ src/test/kotlin/com/project/movienight/controllers/FilmLibraryControllerTest.kt
@@ -0,0 +1,74 @@
+package com.project.movienight.controllers
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.project.movienight.adapters.web.dto.request.CreateFilmRequest
+import com.project.movienight.adapters.web.dto.request.CreateUserRequest
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.transaction.annotation.Transactional
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class FilmLibraryControllerTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+
+    @Test
+    fun `add film to library should work`() {
+        val userRequest = CreateUserRequest("Film Adder", "adder@example.com")
+        val userResponse = mockMvc.perform(
+            post("/api/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userRequest))
+        ).andReturn()
+        val userId = objectMapper.readTree(userResponse.response.contentAsString).get("id").asText()
+
+        val filmRequest = CreateFilmRequest("Library Film", "Film description")
+        val filmResponse = mockMvc.perform(
+            post("/api/films")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(filmRequest))
+        ).andReturn()
+        val filmId = objectMapper.readTree(filmResponse.response.contentAsString).get("id").asText()
+
+        mockMvc.perform(
+            post("/api/users/$userId/library/films/$filmId")
+        )
+            .andExpect(status().isCreated)
+    }
+
+    @Test
+    fun `remove film from library should return 204`() {
+        val userRequest = CreateUserRequest("Remove Film", "remove@example.com")
+        val userResponse = mockMvc.perform(
+            post("/api/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userRequest))
+        ).andReturn()
+        val userId = objectMapper.readTree(userResponse.response.contentAsString).get("id").asText()
+
+        val filmRequest = CreateFilmRequest("Film To Remove", "Will be removed")
+        val filmResponse = mockMvc.perform(
+            post("/api/films")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(filmRequest))
+        ).andReturn()
+        val filmId = objectMapper.readTree(filmResponse.response.contentAsString).get("id").asText()
+
+        mockMvc.perform(post("/api/users/$userId/library/films/$filmId"))
+        mockMvc.perform(delete("/api/users/$userId/library/films/$filmId"))
+            .andExpect(status().isNoContent())
+    }
+}
